b69290228c181749bf90a505d575eac8ea3f2141
jack-fu-tamc <303604625@qq.com>
2016-11-08 08:38:25 +0800

    考生报名信息service和扩展viewresult

---
+++ HC.Service/EnrolSys/EnrolSysService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HC.Data.Models;
+using ResposityOfEf;
+
+
+namespace HC.Service.EnrolSys
+{
+    public class EnrolSysService : IEnrolSysService
+    {
+
+        #region fileds
+        private IRepository<Data.Models.StudentInfo> _StudentInfoRepository;
+        #endregion
+
+        #region cto
+        public EnrolSysService(IRepository<HC.Data.Models.StudentInfo> StudentInfoRepository)
+        {
+            this._StudentInfoRepository = StudentInfoRepository;
+
+        }
+        #endregion
+
+        public ResposityOfEf.IPagedList<Data.Models.StudentInfo> getStudentInfoBySearch(Data.ViewModels.SerchStudentModel sModel, int pageSize, int pageIndex)
+        {
+            var query = _StudentInfoRepository.Table;
+            
+
+            query = query.Where(x => x.RegisterTime >= sModel.sTime && x.RegisterTime <= sModel.eTime);
+
+
+            if (!string.IsNullOrEmpty(sModel.StudentName))
+            {
+                query = query.Where(x => x.StudentName.Contains(sModel.StudentName));
+            }
+            if (!string.IsNullOrEmpty(sModel.StCardNo))
+            {
+                query = query.Where(x => x.SelfCardNo==sModel.StCardNo);
+            }
+            if (!string.IsNullOrEmpty(sModel.StFrom))
+            {
+                query = query.Where(x => x.Sfrom == sModel.StFrom);
+            }
+
+            return new PagedList<HC.Data.Models.StudentInfo>(query.OrderByDescending(x=>x.RegisterTime), pageIndex, pageSize);
+        }
+
+        public Data.Models.StudentInfo GetStudentInfoById(int studentID)
+        {
+            return _StudentInfoRepository.GetById(studentID);
+        }
+    }
+}
---
+++ HC.Web.Framework/VMEJsonResult .cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using HC.Core.Infrastructure;
+using HC.Service.Authentication;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace HC.Web.Framework
+{
+    /// <summary>  
+    /// 通过重载ExecuteResult方法，实现自定义序列化日期的实现  
+    /// </summary>  
+    public class VMEJsonResult : JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+            if (this.Data != null)
+            {
+           
+                // 设置日期序列化的格式  
+               
+                JsonSerializerSettings jsSettings = new JsonSerializerSettings();
+                jsSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                jsSettings.Converters.Add(new IsoDateTimeConverter
+                {
+                    DateTimeFormat = "yyyy'-'MM'-'dd"//DateTimeFormat = "yyyy'-'MM'-'dd' 'HH':'mm':'ss"                     
+                });                
+                response.Write(JsonConvert.SerializeObject(Data, jsSettings));
+            }
+        }
+    }  
+}
